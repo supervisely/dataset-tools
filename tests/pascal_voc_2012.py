91f89f88082c7a7bab36e453b44cc9dd9f5479d2
Maxim Kolomeychenko <maxim@supervise.ly>
2023-05-13 20:58:11 +0100

    finish render script

--- tests/pascal_voc_2012.py
+++ tests/pascal_voc_2012.py
@@ -12,52 +12,54 @@ if sly.is_development():
     load_dotenv(os.path.expanduser("~/ninja.env"))
     load_dotenv("local.env")
 
-api = sly.Api.from_env()
-
 MAX_WIDTH = 500
-project_id = 223
+
+api = sly.Api.from_env()
+project_id = sly.env.project_id()
+workspace_id = sly.env.workspace_id()
+team_id = sly.env.team_id()
 
 project_info = api.project.get_info_by_id(project_id)
 project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
-workspace_id = project_info.workspace_id
-workspace_info = api.workspace.get_info_by_id(workspace_id)
-team_id = workspace_info.team_id
-
-ninja_dir = f"/{workspace_id}/{project_id}/"
+ninja_dir = f"/dataset/{workspace_id}/{project_id}/"
 
 pbar = tqdm(total=project_info.items_count)
-
-# render annotations
 for dataset in api.dataset.get_list(project_id):
     render_dir = os.path.join(ninja_dir, "renders", f"{dataset.id}")
 
+    all_images = api.image.get_list(dataset.id)
     existing_renders = api.file.list2(team_id, render_dir, recursive=False)
     existing_ids = set(int(sly.fs.get_file_name(f.path)) for f in existing_renders)
-    all_images = api.image.get_list(dataset.id)
     new_images: List[sly.ImageInfo] = []
     for image in all_images:
         if image.id not in existing_ids:
             new_images.append(image)
-    pbar.update(existing_ids)
+    pbar.update(len(existing_ids))
 
     for batch in sly.batched(new_images):
         image_ids = [image.id for image in batch]
         anns = api.annotation.download_json_batch(dataset.id, image_ids)
+
+        lpaths = []
+        rpaths = []
         for jann, image in zip(anns, batch):
-            out_size = (image.height, image.width)
+            ann = sly.Annotation.from_json(jann, project_meta)
             if image.width > MAX_WIDTH:
                 out_size = (int((image.height / image.width) * MAX_WIDTH), MAX_WIDTH)
+                ann = ann.resize(out_size)
 
-            ann = sly.Annotation.from_json(jann, project_meta)
-            ann = ann.resize(out_size)
-            render = np.zeros((out_size[0], out_size[1], 3), dtype=np.uint8)
+            render = np.zeros((ann.img_size[0], ann.img_size[1], 3), dtype=np.uint8)
             ann.draw(render, thickness=ann._get_thickness())
 
             local_path = os.path.join(os.getcwd(), "demo", f"{image.id}.png")
             remote_path = os.path.join(render_dir, f"{image.id}.png")
+            lpaths.append(local_path)
+            rpaths.append(remote_path)
             sly.image.write(local_path, render)
-            api.file.upload(team_id, local_path, remote_path)
-            sly.fs.silent_remove(local_path)
-            pbar.update(1)
+
+        api.file.upload_bulk(team_id, lpaths, rpaths)
+        for p in lpaths:
+            sly.fs.silent_remove(p)
+        pbar.update(len(lpaths))
 
 pbar.close()
