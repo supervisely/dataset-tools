454dd6e87056762b0a8a0abf50dd0a188ce4720a
Almaz <79905215+almazgimaev@users.noreply.github.com>
2023-05-13 11:37:33 +0300

    update ClassesPerImage stats (#12)

--- dataset_tools/__init__.py
+++ dataset_tools/__init__.py
@@ -1,3 +1,4 @@
 from dataset_tools import image
 
 from dataset_tools.image.stats.classes import ImgClassesDistribution, ImgClassesCooccurence
+from dataset_tools.image.stats.classes_per_image import ClassesPerImage
