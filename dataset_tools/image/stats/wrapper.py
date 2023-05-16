6288b4f85cb60734e22c72a549422ba573b30914
Ars Vladimirov <grokhael@gmail.com>
2023-05-16 12:52:21 +0600

    project_fs

--- dataset_tools/image/stats/wrapper.py
+++ dataset_tools/image/stats/wrapper.py
@@ -2,11 +2,47 @@ import json
 import os
 import random
 from collections import defaultdict
-from typing import Union
+from typing import Union, List
 
 from tqdm import tqdm
 
 import supervisely as sly
+
+
+def sample_images(
+    api: sly.Api,
+    project: Union[int, str],
+    datasets: List[Union[sly.DatasetInfo, sly.Project.DatasetDict]],
+    sample_rate: float,
+):
+    total = 0
+    samples = []
+    for dataset in datasets:
+        k = int(
+            max(
+                1,
+                sample_rate
+                * (
+                    dataset.items_count
+                    if isinstance(project, int)
+                    else len(os.listdir(dataset.ann_dir))
+                ),
+            )
+        )
+
+        ds_images = (
+            api.image.get_list(dataset.id)
+            if isinstance(project, int)
+            else [
+                dataset.get_image_info(sly.fs.get_file_name(img))
+                for img in os.listdir(dataset.ann_dir)
+            ]
+        )
+
+        s = random.sample(ds_images, k)
+        samples.append((dataset, s))
+        total += k
+    return samples, total
 
 
 def count_stats(
@@ -14,39 +50,32 @@ def count_stats(
 ) -> None:
     if sample_rate <= 0 or sample_rate > 1:
         raise ValueError("Sample rate has to be in range (0, 1]")
+    if api is None:
+        api = sly.Api.from_env()
 
     if isinstance(projeсt, int):
-        if api is None:
-            api = sly.Api.from_env()
-
-        project_info = api.project.get_info_by_id(projeсt, raise_error=True)
         project_meta = sly.ProjectMeta.from_json(api.project.get_meta(projeсt))
-
-        total = 0
-        samples = []
-        for dataset in api.dataset.get_list(projeсt):
-            k = int(max(1, sample_rate * dataset.items_count))
-            ds_images = api.image.get_list(dataset.id)
-            s = random.sample(ds_images, k)
-            samples.append((dataset, s))
-            total += k
-
-        desc = "Calculating stats"
-        if sample_rate != 1:
-            desc += f" [sample={sample_rate}]"
-
-        with tqdm(desc=desc, total=total) as pbar:
-            for dataset, images in samples:
-                for batch in sly.batched(images):
-                    image_ids = [image.id for image in batch]
-                    janns = api.annotation.download_json_batch(dataset.id, image_ids)
-                    for img, jann in zip(batch, janns):
-                        ann = sly.Annotation.from_json(jann, project_meta)
-                        for stat in stats:
-                            stat.update(img, ann)
-                        pbar.update(1)
-
+        datasets = api.dataset.get_list(projeсt)
     elif isinstance(projeсt, str):
-        raise NotImplementedError()
+        project_fs = sly.Project(projeсt, sly.OpenMode.READ)
+        project_meta = project_fs.meta
+        datasets = project_fs.datasets
     else:
-        raise ValueError("TODO explain here")
+        raise ValueError("Project should be either an integer project ID or a string project path.")
+
+    samples, total = sample_images(api, projeсt, datasets, sample_rate)
+    desc = "Calculating stats" + (f" [sample={sample_rate}]" if sample_rate != 1 else "")
+    with tqdm(desc=desc, total=total) as pbar:
+        for dataset, images in samples:
+            for batch in sly.batched(images):
+                image_ids = [image.id for image in batch]
+
+                janns = api.annotation.download_json_batch(
+                    (dataset.id if isinstance(projeсt, int) else batch[0].dataset_id), image_ids
+                )
+
+                for img, jann in zip(batch, janns):
+                    ann = sly.Annotation.from_json(jann, project_meta)
+                    for stat in stats:
+                        stat.update(img, ann)
+                    pbar.update(1)
--- tests/get_stats.py
+++ tests/get_stats.py
@@ -8,16 +8,6 @@ from tqdm import tqdm
 import dataset_tools as dtools
 import supervisely as sly
 
-if sly.is_development():
-    load_dotenv(os.path.expanduser("~/ninja.env"))
-    load_dotenv("local.env")
-
-api = sly.Api.from_env()
-
-PROJECT_ID = sly.env.project_id()
-TEAM_ID = sly.env.team_id()
-PROJECT_PATH = os.environ["LOCAL_DATA_DIR"]
-
 
 import shutil
 
@@ -33,6 +23,7 @@ if sly.is_development():
 
 api = sly.Api.from_env()
 project_id = sly.env.project_id()
+project_path = os.environ["LOCAL_DATA_DIR"]
 project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
 
 cls_perimage = dtools.ClassesPerImage(project_meta)
@@ -44,18 +35,24 @@ obj_sizes = dtools.ObjectSizes(project_meta)
 cls_sizes = dtools.ClassSizes(project_meta)
 
 
+# dtools.count_stats(
+#     project_id,
+#     stats=[cls_perimage, cls_balance, cls_cooc, obj_distrib, obj_sizes, cls_sizes],
+#     sample_rate=0.1,
+# )
+
 dtools.count_stats(
-    project_id,
-    stats=[cls_perimage, cls_balance, cls_cooc, obj_distrib, obj_sizes, cls_sizes],
-    sample_rate=0.1,
+    project_path,
+    stats=[cls_cooc],
+    sample_rate=1,
 )
 
-with open("./demo/class_perimage.json", "w") as f:
-    json.dump(cls_perimage.to_json(), f)
-# cls_perimage.to_image("./demo/class_perimage.png")
-with open("./demo/class_balance.json", "w") as f:
-    json.dump(cls_balance.to_json(), f)
-cls_balance.to_image("./demo/class_balance.png")
+# with open("./demo/class_perimage.json", "w") as f:
+#     json.dump(cls_perimage.to_json(), f)
+# # cls_perimage.to_image("./demo/class_perimage.png")
+# with open("./demo/class_balance.json", "w") as f:
+#     json.dump(cls_balance.to_json(), f)
+# cls_balance.to_image("./demo/class_balance.png")
 with open("./demo/class_cooc.json", "w") as f:
     json.dump(cls_cooc.to_json(), f)
 cls_cooc.to_image("./demo/class_cooc.png")
