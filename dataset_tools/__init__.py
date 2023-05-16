995766dad9c46a8b0c6165bae54620b0b46276fd
almaz_macbook <almazgimaev@gmail.com>
2023-05-16 10:29:05 +0300

    grid with side annotations

--- dataset_tools/__init__.py
+++ dataset_tools/__init__.py
@@ -2,3 +2,7 @@
 from dataset_tools.image.stats.class_balance import ClassBalance
 from dataset_tools.image.stats.classes_per_image import ClassesPerImage
 from dataset_tools.image.stats.wrapper import count_stats  # , initialize
+
+
+from dataset_tools.image.renders.wrapper import prepare_renders
+from dataset_tools.image.renders.separated_anns_grid import SideAnnotationsGrid
