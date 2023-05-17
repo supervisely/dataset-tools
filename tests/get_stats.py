38543a739b5ce481dd466209208bb911af1995c0
Ars Vladimirov <grokhael@gmail.com>
2023-05-17 12:05:38 +0600

    refactor tests/get_stars.py

--- tests/get_stats.py
+++ tests/get_stats.py
@@ -1,62 +1,49 @@
-import io
 import json
 import os
 
 from dotenv import load_dotenv
-from tqdm import tqdm
 
-import dataset_tools as dtools
 import supervisely as sly
-
-
-import shutil
-
-# if os.path.exists(PROJECT_PATH):
-#     shutil.rmtree(PROJECT_PATH)
-
-# sly.download(api, PROJECT_ID, PROJECT_PATH, save_image_info=True, save_images=False)
+import dataset_tools as dtools
 
 if sly.is_development():
     load_dotenv(os.path.expanduser("~/ninja.env"))
     load_dotenv("local.env")
 
+os.makedirs("./stats/", exist_ok=True)
 api = sly.Api.from_env()
+
+# 1. api way
 project_id = sly.env.project_id()
-project_path = os.environ["LOCAL_DATA_DIR"]
 project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
 
-cls_perimage = dtools.ClassesPerImage(project_meta)
-cls_balance = dtools.ClassBalance(project_meta)
-cls_cooc = dtools.ClassCooccurrence(project_meta)
-
-obj_distrib = dtools.ObjectsDistribution(project_meta)
-obj_sizes = dtools.ObjectSizes(project_meta)
-cls_sizes = dtools.ClassSizes(project_meta)
-cls_heatmaps = dtools.ClassesHeatmaps(project_meta)
+# 2. localdir way
+# project_path = os.environ["LOCAL_DATA_DIR"]
+# sly.download(api, project_id, project_path, save_image_info=True, save_images=False)
+# project_meta = sly.Project(project_path, sly.OpenMode.READ).meta
 
 
-dtools.count_stats(
-    project_id,
-    stats=[cls_heatmaps],
-    sample_rate=1,
-)
+def main():
+    stats = [
+        dtools.ClassesPerImage(project_meta),
+        dtools.ClassBalance(project_meta),
+        dtools.ClassCooccurrence(project_meta),
+        dtools.ObjectsDistribution(project_meta),
+        dtools.ObjectSizes(project_meta),
+        dtools.ClassSizes(project_meta),
+    ]
+    # pass project_id or project_path as a first argument
+    dtools.count_stats(
+        project_id,
+        stats=stats,
+        sample_rate=0.01,
+    )
+    print("Saving stats...")
+    for stat in stats:
+        with open(f"./stats/{stat.json_name}.json", "w") as f:
+            json.dump(stat.to_json(), f)
+        stat.to_image(f"./stats/{stat.json_name}.png")
 
-# with open("./demo/class_perimage.json", "w") as f:
-#     json.dump(cls_perimage.to_json(), f)
-# # cls_perimage.to_image("./demo/class_perimage.png")
-# with open("./demo/class_balance.json", "w") as f:
-#     json.dump(cls_balance.to_json(), f)
-# cls_balance.to_image("./demo/class_balance.png")
-# with open("./demo/class_cooc.json", "w") as f:
-#     json.dump(cls_cooc.to_json(), f)
-# cls_cooc.to_image("./demo/class_cooc.png")
-# confusion_matrix = cls_cooc.get_widget()
 
-# with open("./demo/obj_distrib.json", "w") as f:
-#     json.dump(obj_distrib.to_json(), f)
-# with open("./demo/object_sizes.json", "w") as f:
-#     json.dump(obj_sizes.to_json(), f)
-# with open("./demo/class_size.json", "w") as f:
-#     json.dump(cls_sizes.to_json(), f)
-
-cls_heatmaps.to_image("./demo")
+if __name__ == "__main__":
+    main()
