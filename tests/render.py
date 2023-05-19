3ec161d5737f68e0e803ca8ef08af0097e8e87e5
almaz_macbook <almazgimaev@gmail.com>
2023-05-19 13:35:49 +0300

    small changes in render.py

--- tests/render.py
+++ tests/render.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 
 from dotenv import load_dotenv
 
@@ -15,13 +14,12 @@ api = sly.Api.from_env()
 
 # 1. api way
 project_id = sly.env.project_id()
-# project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
+project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
 
 # 2. localdir way
 project_path = os.environ["LOCAL_DATA_DIR"]
 # sly.download(api, project_id, project_path, save_image_info=True, save_images=False)
-project_meta = sly.Project(project_path, sly.OpenMode.READ).meta
-
+# project_meta = sly.Project(project_path, sly.OpenMode.READ).meta
 
 poster = dtools.Poster(project_path, project_meta)
 side_anns_grid = dtools.SideAnnotationsGrid(project_path, project_meta)
@@ -38,5 +36,5 @@ dtools.prepare_renders(
     sample_cnt=40,
 )
 
-for renderer in renderers:
-    renderer.to_image(f"./render_results/{renderer.render_name}.png")
+for r in renderers:
+    r.to_image(f"./render_results/{r.basename_stem}.png")
