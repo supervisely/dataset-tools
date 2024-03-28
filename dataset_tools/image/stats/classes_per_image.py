bf79a9409c6b1141ddee58dd5eabd38514d9f81a
Ars Vladimirov <grokhael@gmail.com>
2024-03-28 20:58:06 +0500

    class_per_iamge: fix rounding

--- dataset_tools/image/stats/classes_per_image.py
+++ dataset_tools/image/stats/classes_per_image.py
@@ -98,7 +98,7 @@ class ClassesPerImage(BaseStats):
         counts = {class_id: 0 for class_id in self._class_ids}
         areas = {class_id: 0 for class_id in self._class_ids}
 
-        image_area = image.height * image.width
+        image_area = float(image.height * image.width)
 
         row = [
             image.name,
@@ -109,7 +109,8 @@ class ClassesPerImage(BaseStats):
 
         for figure in figures:
             counts[figure.class_id] += 1
-            areas[figure.class_id] += round(int(figure.area) / image_area * 100, 2)
+            area_percent = float(figure.area) / image_area * 100
+            areas[figure.class_id] += round(area_percent, 2)
 
         for class_id in self._class_ids:
             row.extend([counts[class_id], areas[class_id]])
@@ -177,7 +178,9 @@ class ClassesPerImage(BaseStats):
                 for cls in cur_class_names[1:]:
                     render_rgb = np.zeros(ann.img_size + (3,), dtype=np.uint8)
 
-                    class_labels = [label for label in ann.labels if label.obj_class.name == cls]
+                    class_labels = [
+                        label for label in ann.labels if label.obj_class.name == cls
+                    ]
                     clann = ann.clone(labels=class_labels)
 
                     clann.draw(render_rgb, [1, 1, 1])
@@ -193,9 +196,12 @@ class ClassesPerImage(BaseStats):
                     total_area = stacked_masks.shape[0] * stacked_masks.shape[1]
                     mask_areas = (np.sum(stacked_masks, axis=(0, 1)) / total_area) * 100
 
-                    mask_areas = np.insert(mask_areas, 0, self.calc_unlabeled_area_in(masks))
+                    mask_areas = np.insert(
+                        mask_areas, 0, self.calc_unlabeled_area_in(masks)
+                    )
                     stat_area = {
-                        cls: area for cls, area in zip(cur_class_names, mask_areas.tolist())
+                        cls: area
+                        for cls, area in zip(cur_class_names, mask_areas.tolist())
                     }
 
                     if self._stat_cache is not None:
@@ -232,9 +238,15 @@ class ClassesPerImage(BaseStats):
                     cur_area = 0
                     cur_count = 0
                 else:
-                    cur_area = stat_area[class_name] if not np.isnan(stat_area[class_name]) else 0
+                    cur_area = (
+                        stat_area[class_name]
+                        if not np.isnan(stat_area[class_name])
+                        else 0
+                    )
                     cur_count = (
-                        stat_count[class_name] if not np.isnan(stat_count[class_name]) else 0
+                        stat_count[class_name]
+                        if not np.isnan(stat_count[class_name])
+                        else 0
                     )
                 table_row.append(cur_count)
                 table_row.append(round(cur_area, 2) if cur_area != 0 else 0)
@@ -312,7 +324,9 @@ class ClassesPerImage(BaseStats):
 
         for file in files:
             loaded_data = np.load(file, allow_pickle=True).tolist()
-            if len(loaded_data[0][0][4:]) != (len(labeled_cls) * 2):  # TODO unlabeled ..[5:]..
+            if len(loaded_data[0][0][4:]) != (
+                len(labeled_cls) * 2
+            ):  # TODO unlabeled ..[5:]..
                 loaded_data = update_shape(loaded_data, updated_classes)
 
             for image in loaded_data:
