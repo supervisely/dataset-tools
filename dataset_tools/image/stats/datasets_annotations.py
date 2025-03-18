735ea693523587ddd095604bba406870ddcba0dc
vorozhkog <vorozhkog@icloud.com>
2025-03-18 00:10:32 +0000

    wip

--- dataset_tools/image/stats/datasets_annotations.py
+++ dataset_tools/image/stats/datasets_annotations.py
@@ -28,8 +28,15 @@ class DatasetsAnnotations(BaseStats):
 
         self._num_annotated = {ds_id: 0 for ds_id in self._id_to_info.keys()}
         self._num_tagged = {ds_id: 0 for ds_id in self._id_to_info.keys()}
+
         self._class_avg_cnt = {}
         self._class_avg_area = {}
+        for ds_id in self._id_to_info.keys():
+            self._class_avg_cnt[ds_id] = {}
+            self._class_avg_area[ds_id] = {}
+            for cls in project_meta.obj_classes:
+                self._class_avg_cnt[ds_id][cls.sly_id] = 0
+                self._class_avg_area[ds_id][cls.sly_id] = 0
 
         self._class_cnt_sum = {}
         self._class_area_sum = {}
@@ -78,28 +85,29 @@ class DatasetsAnnotations(BaseStats):
             return
         self.is_unlabeled = False
 
+        image_area = image.width * image.height
         self._num_annotated[ds_id] += 1
         for figure in figures:
             self._images_set[ds_id].add(image.id)
             self._class_cnt_sum[ds_id][figure.class_id] += 1
-            self._class_area_sum[ds_id][figure.class_id] += int(figure.area)
+            self._class_area_sum[ds_id][figure.class_id] += float(figure.area) / image_area * 100
 
     def to_json(self):
         raise NotImplementedError()
     
     def to_json2(self) -> Optional[Dict]:
-        columns = ["Dataset", "ID", "Total Images Count", "Annotated Images Count", "Tagged Images Count"]
+        columns = ["Dataset", "ID", "Total", "Annotated", "Tagged"]
         for name in self._class_id_to_name.values():
-            columns.append(f"{name} Avg Count")
-            columns.append(f"{name} Avg Area")
+            columns.append(name)
+            columns.append(name)
 
         max_images = max(list(self._id_to_total.values()))
         max_annotated = max(list(self._num_annotated.values()))
         max_tagged = max(list(self._num_tagged.values()))
 
         col_options = [None] * len(columns)
-        col_options[0] = {"type": "dataset"}
-        col_options[1] = {"type": "id"}
+        col_options[0] = {}
+        col_options[1] = {}
         col_options[2] = {"maxValue": max_images, "tooltip": "Total number of images in the dataset"}
         col_options[3] = {"maxValue": max_annotated, "tooltip": "Total number of annotated images"}
         col_options[4] = {"maxValue": max_tagged, "tooltip": "Total number of tagged images"}
@@ -111,28 +119,27 @@ class DatasetsAnnotations(BaseStats):
         }
 
         count_tooltip = "Average count of objects per image"
-        area_tooltip = "Average area of objects per image"
+        area_tooltip = "Average area covered by objects per image"
         rows, refs = [], []
         for ds_id, ds_name in self._id_to_name.items():
-            for idx, class_id in enumerate(self._class_cnt_sum[ds_id]):
-                col_options[5 + 2 * idx] = {"maxValue": max(list(self._class_cnt_sum[ds_id].values())), "subtitle": "average", "tooltip": count_tooltip}
-                col_options[6 + 2 * idx] = {"maxValue": max(list(self._class_area_sum[ds_id].values())), "subtitle": "average", "tooltip": area_tooltip,"postfix": "%"}
-                try:
-                    self._class_avg_cnt[class_id] = self._class_cnt_sum[ds_id][class_id] / self._num_annotated[ds_id]
-                except ZeroDivisionError:
-                    self._class_avg_cnt[class_id] = 0
-                try:
-                    self._class_avg_area[class_id] = self._class_area_sum[ds_id][class_id] / self._num_annotated[ds_id]
-                except ZeroDivisionError:
-                    self._class_avg_area[class_id] = 0
-
             total = self._id_to_total[ds_id]
             num_ann = self._num_annotated[ds_id]
             num_tag = self._num_tagged[ds_id]
             row = [ds_name, ds_id, total, num_ann, num_tag]
-            for class_id in self._class_id_to_name.keys():
-                row.append(self._class_avg_cnt[class_id])
-                row.append(self._class_avg_area[class_id])
+            for idx, class_id in enumerate(self._class_id_to_name.keys()):
+                self._class_avg_cnt[ds_id][class_id] = (
+                    round(self._class_cnt_sum[ds_id][class_id] / num_ann, 2) if num_ann else 0
+                )
+                self._class_avg_area[ds_id][class_id] = (
+                    round(self._class_area_sum[ds_id][class_id] / num_ann, 2) if num_ann else 0
+                )
+                max_value_per_class_cnt = max([cnt for class_to_cnt in self._class_avg_cnt.values() for clss, cnt in class_to_cnt.items() if clss == class_id])
+                max_value_per_class_area = max([area for class_to_area in self._class_avg_area.values() for clss, area in class_to_area.items() if clss == class_id])
+                col_options[5 + 2 * idx] = {"maxValue": max_value_per_class_cnt, "subtitle": "objects/image on average", "tooltip": count_tooltip}
+                col_options[6 + 2 * idx] = {"maxValue": max_value_per_class_area, "subtitle": "average covered area", "tooltip": area_tooltip, "postfix": "%"}
+
+                row.append(self._class_avg_cnt[ds_id][class_id])
+                row.append(self._class_avg_area[ds_id][class_id])
             rows.append(row)
 
             seized_refs = self._seize_list_to_fixed_size(list(self._images_set[ds_id]), 1000)
@@ -152,11 +159,13 @@ class DatasetsAnnotations(BaseStats):
         images_set = np.array(self._images_set, dtype=object)
         avg_class_cnt_sum = np.array(self._class_cnt_sum, dtype=object)
         avg_class_area_sum = np.array(self._class_area_sum, dtype=object)
+        num_annotated = np.array(self._num_annotated, dtype=object)
         return np.stack(
             [
                 images_set,
                 avg_class_cnt_sum,
                 avg_class_area_sum,
+                num_annotated
             ],
             axis=0,
         )
@@ -174,17 +183,22 @@ class DatasetsAnnotations(BaseStats):
                     loaded_data[0][ds_id] = set()
                     loaded_data[1][ds_id] = {class_id: 0 for class_id in self._class_id_to_name.keys()}
                     loaded_data[2][ds_id] = {class_id: 0 for class_id in self._class_id_to_name.keys()}
+                    loaded_data[3][ds_id] = 0
 
                 removed = loaded_ds_ids.difference(true_ds_ids)
                 for ds_id in list(removed):
                     loaded_data[0].pop(ds_id)
                     loaded_data[1].pop(ds_id)
                     loaded_data[2].pop(ds_id)
+                    loaded_data[3].pop(ds_id)
 
                 save_data = np.array(loaded_data, dtype=object)
                 np.save(file, save_data)
 
-                for ds_id in self._id_to_name.keys():
+                for ds_id in loaded_ds_ids:
+                    self._num_annotated[ds_id] += loaded_data[3][ds_id]
                     self._images_set[ds_id].update(loaded_data[0][ds_id])
-                    self._class_cnt_sum[ds_id] = loaded_data[1][ds_id]
-                    self._class_area_sum[ds_id] = loaded_data[2][ds_id]
+                    for class_id, cnt in loaded_data[1][ds_id].items():
+                        self._class_cnt_sum[ds_id][class_id] += cnt
+                    for class_id, area in loaded_data[2][ds_id].items():
+                        self._class_area_sum[ds_id][class_id] += area
