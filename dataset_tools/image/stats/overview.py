fceba0af0aad3badda190bda398e5cc2c5e01c59
vorozhkog <115161827+vorozhkog@users.noreply.github.com>
2025-06-10 02:19:19 +0100

    Tweak Overview classes styling (#77)
    * simplify class, add experimental settings
    
    * add dataLabels and plotOptions to json
    
    * reduce offset

--- dataset_tools/image/stats/overview.py
+++ dataset_tools/image/stats/overview.py
@@ -13,7 +13,8 @@ class OverviewPie(BaseStats):
     It creates a pie chart showing the ratio between annotated and unlabeled images.
     """
 
-    CHART_HEIGHT = 200
+    CHART_HEIGHT = 250
+    BORDER_WIDTH = 0
     def __init__(self, project_meta: sly.ProjectMeta, project_stats: Dict, force: bool = False,
                  stat_cache: dict = None) -> None:
         self._meta = project_meta
@@ -23,6 +24,8 @@ class OverviewPie(BaseStats):
 
         self._series = []
         self._refs = defaultdict(list)
+        self._type = "pie"
+        self._colors = None
 
         self._update_chart()
         
@@ -42,20 +45,46 @@ class OverviewPie(BaseStats):
         raise NotImplementedError()
 
     def update2(self, image: ImageInfo, figures: List[FigureInfo]):
-        if len(figures) == 0:
-            self._refs["Unlabeled"].append(image.id)
-        else:
-            self._refs["Annotated"].append(image.id)
+        key = "Annotated" if len(figures) > 0 else "Unlabeled"
+        self._refs[key].append(image.id)
 
     def to_json(self):
         raise NotImplementedError()
 
     def to_json2(self):
-        pie_json = PieChart("", self._series, 3, True, self.CHART_HEIGHT, "pie").get_json_data()
-        pie_json['options'].pop('dataLabels')
-        pie_json['options']['chart']['height'] = self.CHART_HEIGHT
-        pie_json['referencesCell'] = self._seize_list_to_fixed_size(self._refs, 1000)
-        return pie_json
+        chart = PieChart("", self._series, self.BORDER_WIDTH, True, self.CHART_HEIGHT, self._type)
+        if self._colors is not None:
+            chart.set_colors(self._colors)
+
+        chart_json = chart.get_json_data()
+
+        chart_json["options"]["dataLabels"] = {
+            "enabled": True,
+            "style": {
+                "fontSize": "16px",
+                "fontWeight": "550",
+                "colors": ["black"]
+            },
+            "dropShadow": {"enabled": False}
+        }
+        chart_json["options"]["plotOptions"] = {
+            "pie": {
+			    "customScale": 0.8,
+				"dataLabels": {
+					"offset": 45
+				}
+			},
+			"donut": {
+			    "size": "80%",
+				"labels": {
+					"show": False
+				}
+			}
+        }
+
+        chart_json["options"]["chart"]["height"] = self.CHART_HEIGHT
+        chart_json["referencesCell"] = self._seize_list_to_fixed_size(self._refs, 1000)
+        return chart_json
         
     def to_numpy_raw(self):
         return np.array([
@@ -81,6 +110,7 @@ class OverviewDonut(OverviewPie):
     def __init__(self, project_meta: sly.ProjectMeta, project_stats: Dict, force: bool = False,
                  stat_cache: dict = None) -> None:
         super().__init__(project_meta, project_stats, force, stat_cache)
+        self._type = "donut"
         self._colors = [item.color for item in self._meta.obj_classes.items()]
         self._class_id_to_name = {item.sly_id: item.name for item in self._meta.obj_classes.items()}
         self._update_chart()
@@ -97,12 +127,3 @@ class OverviewDonut(OverviewPie):
     def update2(self, image: ImageInfo, figures: List[FigureInfo]):
         for figure in figures:
             self._refs[self._class_id_to_name[figure.class_id]].append(image.id)
-
-    def to_json2(self):
-        donut = PieChart("", self._series, 3, True, self.CHART_HEIGHT, "donut")
-        donut.set_colors(self._colors)
-        donut_json = donut.get_json_data()
-        donut_json['options'].pop('dataLabels')
-        donut_json['options']['chart']['height'] = self.CHART_HEIGHT
-        donut_json['referencesCell'] = self._seize_list_to_fixed_size(self._refs, 1000)
-        return donut_json
